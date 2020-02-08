ef9ebb6cfe4225becdce56ab17a097b3b762bbf7
kazukazu123123 <50506519+kazukazu123123@users.noreply.github.com>
2020-02-08 09:35:01 +0900

    Delete test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /home/ced/java-text
-javac -Xdiags:verbose -sourcepath src -d classes src/texteditor/Main.java
-jar cvfm texteditor.jar META-INF/MANIFEST.MF -C classes . resources/*
-java -jar texteditor.jar
